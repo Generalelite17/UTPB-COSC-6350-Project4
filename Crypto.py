74fa8f092c8a83cb0f135ce7bd9917706be80944
Emmanuel Cardenas <117332577+Generalelite17@users.noreply.github.com>
2024-12-13 22:28:55 -0600

    Update Crypto.py
    Remove duplicated aes_decryption method

--- Crypto.py
+++ Crypto.py
@@ -5,31 +5,16 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
 import os
 
-# AES Encryption
+# AES-GCM Encryption
 def aes_gcm_encrypt(plaintext, key):
-    iv = os.urandom(12)  # # 12-byte nonce
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
-    encryptor = cipher.encryptor()
-    padder = padding.PKCS7(128).padder()
-    padded_data = padder.update(plaintext.encode()) + padder.finalize()
-    ciphertext = encryptor.update(padded_data) + encryptor.finalize()
-    return iv + ciphertext  # Return IV + ciphertext
-
-# AES Decryption
-def aes_gcm_encrypt(plaintext, key):
-    """
-    Encrypt plaintext using AES-GCM.
-    """
     nonce = os.urandom(12)  # 12-byte nonce
     cipher = Cipher(algorithms.AES(key), modes.GCM(nonce), backend=default_backend())
     encryptor = cipher.encryptor()
     ciphertext = encryptor.update(plaintext.encode()) + encryptor.finalize()
     return nonce, ciphertext, encryptor.tag
 
+# AES-GCM Decryption
 def aes_gcm_decrypt(nonce, ciphertext, tag, key):
-    """
-    Decrypt ciphertext using AES-GCM.
-    """
     cipher = Cipher(algorithms.AES(key), modes.GCM(nonce, tag), backend=default_backend())
     decryptor = cipher.decryptor()
     plaintext = decryptor.update(ciphertext) + decryptor.finalize()
